8a2aa4dcd1e84af2a708ed4da24ffa9f0872bc0d
Karen Lei <karenwinnielei@gmail.com>
2021-03-17 23:09:43 -0700

    add styling to buttons and cards, add profile pic url to student data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
+import { mockSearchData, mockStudentData } from './mockData';
+import './blueprint.css';
+import { Button, Card, Elevation } from '@blueprintjs/core';
+import { useState } from 'react';
+
 import './App.css';
 
-function App() {
+function App(props) {
+  const [activeFilter, setActiveFilter] = useState();
+
+  const [displayedStudents, setDisplayedStudents] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const [isGreen, setGreen] = useState(false);
+
+  const handleClick = (id) => {
+    const result = mockStudentData.filter((data) => {
+      return data.filterIds.includes(id);
+    });
+
+    const greenButton = mockSearchData.filter((data) => {
+      return data._id.includes(id);
+    });
+
+    // console.log('green', greenButton);
+    // console.log(id);
+
+    setDisplayedStudents(result);
+    setActiveFilter(id);
+
+    if (id) {
+      setGreen(!isGreen);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='filter-container'>
+        {mockSearchData.map((filter, index) => {
+          if (Math.floor(index / 6) + 1 === currentPage) {
+            return (
+              <Button
+                style={
+                  isGreen
+                    ? { backgroundColor: 'green' }
+                    : { backgroundColor: 'white' }
+                }
+                className='filter-btn'
+                onClick={() => handleClick(filter._id)}
+              >
+                {filter.emoticon} {filter.title}
+              </Button>
+            );
+          }
+        })}
+      </div>
+
+      <div className='page-btn'>
+        <Button
+          icon='chevron-left'
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        ></Button>
+        <Button
+          icon='chevron-right'
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={
+            currentPage === Math.floor(mockSearchData.length / 6) + 1
+          }
+        ></Button>
+      </div>
+
+      <div>
+        Page {currentPage} of{' '}
+        {Math.floor(mockSearchData.length / 6) + 1} results
+      </div>
+      <div className='card-container'>
+        {displayedStudents.map((student) => (
+          <Card className='student-cards bp3-elevation-2'>
+            <div className='photo-container'>
+              <img src={student.profilePic} />
+            </div>
+            <div className='text-container'>
+              <div style={{ fontWeight: 'bold' }}>{student.name}</div>
+              <div>{student.university}</div>
+              <div>GPA: {student.gpa}</div>
+              <div>Enrolled in {student.enroledPrograms}</div>
+              <div>Completed {student.completedPrograms}</div>
+              <div>Intent Score: {student.intent}</div>
+              <div>Skill Score: {student.skillPoints}</div>
+            </div>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/mockData.js
@@ -0,0 +1,89 @@
+const mockSearchData = [
+  {
+    _id: 'f1',
+    title: 'Filter 1',
+    emoticon: '📚',
+  },
+  {
+    _id: 'f2',
+    title: 'Filter 2',
+    emoticon: '🧭',
+  },
+  {
+    _id: 'f3',
+    title: 'Filter 3',
+    emoticon: '🖊️'
+  },
+  {
+    _id: 'f4',
+    title: 'Filter 4',
+    emoticon: '🎒',
+  },
+  {
+    _id: 'f5',
+    title: 'Filter 5',
+    emoticon: '📝',
+  },
+  {
+    _id: 'f6',
+    title: 'Filter 6',
+    emoticon: '🏁',
+  },
+  {
+    _id: 'f7',
+    title: 'Filter 7',
+    emoticon: '📎',
+  },
+  {
+    _id: 'f8',
+    title: 'Filter 8',
+    emoticon: '💡',
+  },
+];
+
+const mockStudentData = [
+  {
+    _id: 's1',
+    filterIds: ['f1', 'f3', 'f4'],
+    name: 'Student 1',
+    university: 'Uni-Syd-1',
+    gpa: 3.4,
+    enroledPrograms: ['Program X'],
+    completedPrograms: [],
+    intent: 100,
+    skillPoints: 89,
+    profilePic:
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+  },
+  {
+    _id: 's2',
+    filterIds: ['f1', 'f2', 'f5'],
+    name: 'Student 2',
+    university: 'Uni-Syd-2',
+    gpa: 3.5,
+    enroledPrograms: ['Program Y'],
+    completedPrograms: ['Program A'],
+    intent: 90,
+    skillPoints: 94,
+    profilePic:
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+  },
+  {
+    _id: 's3',
+    filterIds: ['f1', 'f3', 'f6'],
+    name: 'Student 3',
+    university: 'Uni-Syd-3',
+    gpa: 3.0,
+    enroledPrograms: ['Program Z'],
+    completedPrograms: [],
+    intent: 88,
+    skillPoints: 92,
+    profilePic:
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+  },
+];
+
+export {
+  mockSearchData,
+  mockStudentData,
+};
